204b0745e368d51152d4405983b609242951bdd6
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2024-12-29 17:59:13 -0300

    exercises 2.9

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
 
 import Header from './components/Header'
+import Input from './components/Input'
+import Button from './components/Button'
 
 const App = () => {
+  // personas
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123'}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  // datos para nuevo contacto
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  // filtro por nombre
+  const [filterName, setFilterName] = useState('')
 
   const verifyNewName = () => {
     if (persons.some(person => person.name === newName)) {
@@ -15,7 +24,7 @@ const App = () => {
       alert(`'${newName}' is already added to phonebook`)
       return false
     }
-    if (!newName || newName.length === 0) {
+    if (!newName || newName.trim().length === 0) {
       console.log('nombre nulo')
       alert(`enter a valid name to add`)
       return false
@@ -29,7 +38,7 @@ const App = () => {
       alert(`'${newNumber}' is already added to phonebook`)
       return false
     }
-    if (!newNumber || newNumber.length === 0) {
+    if (!newNumber || newNumber.trim().length === 0) {
       console.log('numero nulo')
       alert(`enter a valid number to add`)
       return false
@@ -43,7 +52,8 @@ const App = () => {
     if (!verifyNewNumber()) return;
     const contactObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
     setPersons(persons.concat(contactObject))
     setNewName('')
@@ -58,20 +68,24 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilterName(event.target.value)
+  }
+
+  const personsToShow = (filterName.trim().length > 0) ? persons.filter(person => person.name.toLowerCase().includes(filterName.toLowerCase())) : persons
+
   return (
     <div>
       <Header text='Phonebook'></Header>
+      <Input text='filter shown with' value={filterName} onChange={handleFilterChange}></Input>
+      <Header text='add a new'></Header>
       <form>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-          <button onClick={addContact} type="submit"> add </button>
+        <Input text='name' value={newName} onChange={handleNameChange}></Input>
+        <Input text='number' value={newNumber} onChange={handleNumberChange}></Input>
+        <Button text='add' onClick={addContact} type='submit'></Button>
       </form>
       <Header text='Numbers'></Header>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      {personsToShow.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 }
--- phonebook/src/main.jsx
+++ phonebook/src/main.jsx
@@ -2,5 +2,5 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-// exercise 2.8
+// exercise 2.9
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
