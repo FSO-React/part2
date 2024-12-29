87f523ace69f4845b47e3d3e2014652aad0c2eb5
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2024-12-29 09:48:45 -0300

    exercises 2.2 && 2.3

--- courses/src/components/Course.jsx
+++ courses/src/components/Course.jsx
@@ -1,11 +1,13 @@
 import Header from './Header';
 import Content from './Content';
+import Total from './Total';
 
 const Course = ({ name, parts }) => {                                 
     return (
         <div>
             <Header text={name}></Header>
             <Content parts={parts}></Content>
+            <Total parts={parts}></Total>
         </div>
     )
 }
