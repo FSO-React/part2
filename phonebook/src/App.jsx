a8e9e07c53a2ede301b54ff1bca097991c6c4e1c
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2024-12-29 16:51:09 -0300

    exercises 2.6

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,24 +1,46 @@
 import { useState } from 'react'
 
+import Header from './components/Header'
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    if (!newName || newName === 0) return;
+    const nameObject = {
+      name: newName
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <Header text='Phonebook'></Header>
       <form>
         <div>
-          name: <input />
+          name: 
+          <input
+            value={newName} 
+            onChange={handleNameChange} 
+          />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button
+            onClick={addName} 
+            type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      ...
+      <Header text='Numbers'></Header>
+      {persons.map(person => <div key={person.name}>{person.name}</div>)}
     </div>
   )
 }
