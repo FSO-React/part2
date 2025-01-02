add1bb6dcbb49139f0f7fbde32b4fa893738d524
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-01-01 22:53:57 -0300

    exersice 2.11

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Header from './components/Header'
 import Input from './components/Input'
@@ -8,17 +9,21 @@ import PersonsForm from './components/PersonsForm'
 
 const App = () => {
   // personas
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   // datos para nuevo contacto
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   // filtro por nombre
   const [filterName, setFilterName] = useState('')
+
+  const fetchPersons = async () => {
+    try {
+      const { status, data } = await axios.get('http://localhost:3001/persons');
+      if (status === 200) setPersons(data);
+    } catch (error) {
+      console.error('Error fetching persons:', error);
+    }
+  };
 
   const verifyNewName = () => {
     if (persons.some(person => person.name === newName)) {
@@ -73,6 +78,10 @@ const App = () => {
   const handleFilterChange = (event) => {
     setFilterName(event.target.value)
   }
+
+  useEffect(() => {
+    fetchPersons();
+  }, []);
 
   return (
     <div>
